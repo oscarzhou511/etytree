cf701168e1d034ef2fb51cb26e9a296ce4336d25
Oscar Zhou <75863408+oscarzhou511@users.noreply.github.com>
2026-06-09 12:49:29 +0930

    Fix typo in description text

--- components/MainForm.tsx
+++ components/MainForm.tsx
@@ -101,7 +101,7 @@ export function MainForm() {
           <Text inherit variant="gradient" component="span">
             tree
           </Text>
-          <Text className={classes.description}>Visualize Etmological Relaitons</Text>
+          <Text className={classes.description}>Visualize Etmological Relations</Text>
         </Title>
       </Container>
 
